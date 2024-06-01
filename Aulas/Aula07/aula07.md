b928c5db94978fefda3964364366de8bebcb154a
bieldsena1 <gabrieldsenaeduardo@gmail.com>
2024-06-01 11:48:47 -0300

    Aula 7

---
+++ Aulas/Aula07/aula07.md
@@ -0,0 +1,2 @@
+#  Laços de repetição em JS
+[Laços e itereções JS](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Loops_and_iteration)
---
+++ Aulas/Aula07/js/for.js
@@ -0,0 +1,14 @@
+var tarefas = [ 'lavar roupa', 'lavar louca', 'lavar banheiro', 'lavar quintal', 'dar banho no cachorro']
+
+// for(var i = 0; i < tarefas.lenght; i++){
+// console.log(tarefas[i])    
+//}
+
+for (var tarefa of tarefas){
+    console.log(tarefa)
+}
+
+// tarefas.forEach(tarefa =>){
+//    console.log(tarefa)
+//})
+
